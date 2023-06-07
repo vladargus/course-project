a4f5c314667169244acee1b61b84f74418772384
Vladislav <vlad.argus@gmail.com>
2023-06-07 21:04:31 +0300

    Redux final

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -5,7 +5,7 @@ import UserCard from "../../ui/userCard";
 import QualitiesCard from "../../ui/qualitiesCard";
 import MeetingsCard from "../../ui/meetingsCard";
 import Comments from "../../ui/comments";
-import { CommentsProvider } from "../../../hooks/useComments";
+
 import { getUserById } from "../../../store/users";
 
 const UserPage = ({ userId }) => {
@@ -21,9 +21,7 @@ const UserPage = ({ userId }) => {
                         <MeetingsCard value={user.completedMeetings} />
                     </div>
                     <div className="col-md-8">
-                        <CommentsProvider>
-                            <Comments />
-                        </CommentsProvider>
+                        <Comments />
                     </div>
                 </div>
             </div>
